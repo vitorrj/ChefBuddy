f6de5be9bbb8029b209fbe80068568580b1e1f45
Valerio Volpe <kyuubibyte@live.it>
2019-11-20 15:36:01 +0100

    addSupplierView

---
+++ ChefBuddy/Suppliers/addSupplierViewController.swift
@@ -0,0 +1,30 @@
+//
+//  addSupplierViewController.swift
+//  ChefBuddy
+//
+//  Created by Valerio Volpe on 20/11/2019.
+//  Copyright © 2019 Foodies. All rights reserved.
+//
+
+import UIKit
+
+class addSupplierViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
